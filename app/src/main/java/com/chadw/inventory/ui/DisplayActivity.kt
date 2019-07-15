af994bb4be83b5e30889245465577f5553a58acf
Chad Wise <chadharrisonwise@hotmail.com>
2019-07-14 22:26:02 -0400

    added landscape function for display activity

--- app/src/main/java/com/chadw/inventory/ui/DisplayActivity.kt
+++ app/src/main/java/com/chadw/inventory/ui/DisplayActivity.kt
@@ -3,12 +3,26 @@ package com.chadw.inventory.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.chadw.inventory.R
+import com.chadw.inventory.model.Item
+import kotlinx.android.synthetic.main.activity_display.*
 
 class DisplayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
-    }
 
+    }
+    fun updateNetProfit(){
+       var profit: Double? = null
+        profit = askingPriceField.text.toString().toDouble() - totalPriceField.text.toString().toDouble()
+        val profitString :String = "$" + profit.toString()
+        netProfitField.setText(profitString)
+    }
+    fun updateTotalCost(){
+        var totalCost: Double? = null
+        totalCost = purchPriceField.text.toString().toDouble() + matPriceField.text.toString().toDouble() + laborChargeField.text.toString().toDouble()
+        val totalCostString = "$" + totalCost.toString()
+        totalPriceField.setText(totalCostString)
+    }
 }
