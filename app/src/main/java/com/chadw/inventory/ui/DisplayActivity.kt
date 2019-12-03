96aa1f85280c75438e490d69709231b0e8313b0c
Chad Wise <chadharrisonwise@hotmail.com>
2019-12-03 14:25:44 -0500

    Added firebase compatibility

--- app/src/main/java/com/chadw/inventory/ui/DisplayActivity.kt
+++ app/src/main/java/com/chadw/inventory/ui/DisplayActivity.kt
@@ -1,6 +1,6 @@
 package com.chadw.inventory.ui
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chadw.inventory.R
 import com.chadw.inventory.model.Item
--- app/src/main/java/com/chadw/inventory/ui/MainActivity.kt
+++ app/src/main/java/com/chadw/inventory/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.chadw.inventory.ui
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chadw.inventory.R
 import kotlinx.android.synthetic.main.activity_main.*
